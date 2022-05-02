327b04e8a00d546ff6fc38eec9bd7589b5c931bb
Nath <nathaniel@aims.ac.za>
2022-05-02 16:04:10 +0000

    Fixed codes for product-service

--- product-service/src/main/java/com/minejava/productservice/service/ProductService.java
+++ product-service/src/main/java/com/minejava/productservice/service/ProductService.java
@@ -1,15 +1,20 @@
 package com.minejava.productservice.service;
 
+import java.util.List;
+
 import com.minejava.productservice.dto.ProductRequest;
+import com.minejava.productservice.dto.ProductResponse;
 import com.minejava.productservice.model.Product;
 import com.minejava.productservice.repository.ProductRepository;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -24,5 +29,21 @@ public class ProductService {
         
         // Finally save
         productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+    //
+    private ProductResponse mapToProductResponse(Product product){
+        return ProductResponse.builder()
+                        .id(product.getId())
+                        .name(product.getName())
+                        .description(product.getDescription())
+                        .price(getPrice())
+                        .build();
     }
 }
