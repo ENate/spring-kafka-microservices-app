f8e13ab748d47d56355ab261f645bdf77477125d
Nath <nathaniel@aims.ac.za>
2022-05-04 11:56:59 +0000

    Added test for product service

--- product-service/src/main/java/com/minejava/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/minejava/productservice/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.minejava.productservice.repository;
 
 import com.minejava.productservice.model.Product;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
