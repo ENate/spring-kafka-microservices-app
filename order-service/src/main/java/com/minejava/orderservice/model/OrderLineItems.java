a2007fe983ce6fd80e3133b569421ba1773e1f75
Nath <nathaniel@aims.ac.za>
2022-05-06 14:56:16 +0000

    Added more objects to orderservice

---
+++ order-service/src/main/java/com/minejava/orderservice/model/OrderLineItems.java
@@ -0,0 +1,23 @@
+package com.minejava.orderservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+}
