42104f05ac07504cece60e5a11a02d692507c907
Nath <nathaniel@aims.ac.za>
2022-05-07 17:08:36 +0000

    Removed old services folders

--- microservice-parent/order-service/src/main/java/com/minejava/orderservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minejava.orderservice.repository;
-
-import com.minejava.orderservice.model.Order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    
-}
--- microservice-parent/product-service/src/main/java/com/minejava/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.minejava.productservice.repository;
-
-import com.minejava.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    
-}
