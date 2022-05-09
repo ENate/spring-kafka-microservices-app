6e8dff32efbefba97537188e9fefe1bdc2a8f449
Nath <nathaniel@aims.ac.za>
2022-05-09 10:08:31 +0000

    Added inital comments

--- ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/OrderServiceApplication.java
+++ ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/OrderServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderServiceApplication {
-	// checking for initial runs
+	// checking for initial runs for the new account
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
