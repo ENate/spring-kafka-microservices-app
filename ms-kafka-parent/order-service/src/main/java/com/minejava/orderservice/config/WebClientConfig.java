6d0a927ef013a436c1d0a5b481b1e092dbf7c826
MHipo <manes.hipocrates@gmail.com>
2022-05-12 10:32:42 +0000

    Added load balancer for auto services

--- ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/config/WebClientConfig.java
+++ ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.minejava.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
     
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    @LoadBalanced // adds load balancing capabilities: auto create client load
+    // balance service if multiple instances of services is available
+    public WebClient.Builder webClientBuiler(){
+        return WebClient.builder();
     }
 }
--- ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/service/OrderService.java
+++ ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuiler;
 
     public void placeOrder(OrderRequest orderRequest) {
         // Create order object
@@ -42,8 +42,8 @@ public class OrderService {
                                 .map(OrderLineItems::getSkuCode)
                                 .toList();
 
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8083/api/inventory", 
+        InventoryResponse[] inventoryResponseArray = webClientBuiler.build().get()
+                .uri("http://inventory-service/api/inventory",
                 uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
