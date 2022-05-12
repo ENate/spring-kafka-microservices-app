7d66947cb3eb21b05a39510464d80cb35b91135d
MHipo <manes.hipocrates@gmail.com>
2022-05-12 10:21:33 +0000

    Added more configs for client services

--- ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/OrderServiceApplication.java
+++ ms-kafka-parent/order-service/src/main/java/com/minejava/orderservice/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.minejava.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 	// checking for initial runs for the new account
 	public static void main(String[] args) {
